b71256cb4208b4edc942e0c84369bad2f7d58364
Ivan Stanevich <ivan_stanevich@waters.com>
2024-02-11 11:12:14 +0100

    implemet snapshot testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,18 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
-  // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const initialArray = Array.from({ length: 2 }, () => 1);
+
+    expect(generateLinkedList(initialArray)).toStrictEqual({
+      next: { next: { next: null, value: null }, value: 1 },
+      value: 1,
+    });
   });
 
-  // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const initialArray = Array.from({ length: 2 }, () => 2);
+
+    expect(generateLinkedList(initialArray)).toMatchSnapshot();
   });
 });
